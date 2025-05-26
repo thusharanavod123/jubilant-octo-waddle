d2efce5c8cb6fc11249892f02fd583c86dcea8db
Thushara Navod <146652282+thusharanavod123@users.noreply.github.com>
2025-05-26 20:53:34 +0530

    blog post added

--- jubilant-octo-waddle/src/app/layout.tsx
+++ jubilant-octo-waddle/src/app/layout.tsx
@@ -1,3 +1,7 @@
+import './globals.css'; // or '../styles/globals.css'
+
+
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -11,6 +15,10 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+
+
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
